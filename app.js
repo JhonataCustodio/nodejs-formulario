1e9705358f9c65d5b55d2f497a0f12d88725a900
JhonataCustodio <silvajhonata@live.com>
2021-05-09 13:22:54 -0300

    nodejs-formulario ii

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
